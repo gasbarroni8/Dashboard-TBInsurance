73c50d0590c60fc491831ef2753374eda5bef3be
hadesxiong <37078530+hadesxiong@users.noreply.github.com>
2019-12-09 14:48:25 +0800

    Scrapy-Spider
    Update 2019.12.9

--- TaobaoInsurance/TaobaoInsurance/items.py
+++ TaobaoInsurance/TaobaoInsurance/items.py
@@ -26,118 +26,147 @@ class TaobaoinsuranceItem(scrapy.Item):
 
 class SellerInfoItem(scrapy.Item):
 
-    is_seller = scrapy.Field()  # 管道识别，Number
+    is_seller = scrapy.Field()  # 管道识别，number
 
     '''version1'''
 
-    # _id = scrapy.Field()  # 店铺编号，String
-    # seller_nick = scrapy.Field()  # 店铺名称，String
-    # seller_comp = scrapy.Field()  # 店铺公司，String
-    # seller_group = scrapy.Field()  # 店铺所属集团，String
-    # seller_class = scrapy.Field()  # 店铺分类，Number（1自营2经纪）
+    # _id = scrapy.Field()  # 店铺编号，string
+    # seller_nick = scrapy.Field()  # 店铺名称，string
+    # seller_comp = scrapy.Field()  # 店铺公司，string
+    # seller_group = scrapy.Field()  # 店铺所属集团，string
+    # seller_class = scrapy.Field()  # 店铺分类，number（1自营2经纪）
 
     # product_data = scrapy.Field()  # 店铺售卖产品，Object
 
     '''version2'''
 
-    is_seller = scrapy.Field()  #管道识别，Number
+    is_seller = scrapy.Field()  #管道识别，number
 
-    seller_id = scrapy.Field()  #店铺编号，String
-    seller_name = scrapy.Field()  #店铺昵称，String
-    seller_comp = scrapy.Field()  #公司名称，String
-    seller_group = scrapy.Field()  #集团名称，String
-    seller_class = scrapy.Field()  #公司分类，String
+    seller_id = scrapy.Field()  #店铺编号，string
+    seller_name = scrapy.Field()  #店铺昵称，string
+    seller_comp = scrapy.Field()  #公司名称，string
+    seller_group = scrapy.Field()  #集团名称，string
+    seller_class = scrapy.Field()  #公司分类，string
     
-    product_list = scrapy.Field()  #产品列表，Object(list)
-    histroy_list = scrapy.Field()  #产品历史，Object(list)
-    
+    product_list = scrapy.Field()  #产品列表，array
+    histroy_list = scrapy.Field()  #产品历史，array
 
 
-'''商品信息'''
-
+'''商品信息&商品历史信息'''
 
 class ProductInfoItem(scrapy.Item):
 
     '''version1'''
 
-    # is_productList = scrapy.Field()  # 管道识别，Number
-    # is_productInfo = scrapy.Field()  #管道识别，Number
+    # is_productList = scrapy.Field()  # 管道识别，number
+    # is_productInfo = scrapy.Field()  #管道识别，number
     
-    # _id = scrapy.Field()  # 商品编号，String
-    # product_name = scrapy.Field()  # 商品名称，String
-    # product_url = scrapy.Field()  # 商品地址，String
-    # product_minprice = scrapy.Field()  # 商品最低价格，String
-    # product_maxprice = scrapy.Field()  # 商品最高价格，String
-    # product_detail = scrapy.Field()  # 商品详情，String
-    # product_collected = scrapy.Field()  # 商品收藏，Number
-    # product_tags = scrapy.Field()  # 商品标签，String
-    # product_buylimit = scrapy.Field()  #购买限制，Number
+    # _id = scrapy.Field()  # 商品编号，string
+    # product_name = scrapy.Field()  # 商品名称，string
+    # product_url = scrapy.Field()  # 商品地址，string
+    # product_minprice = scrapy.Field()  # 商品最低价格，string
+    # product_maxprice = scrapy.Field()  # 商品最高价格，string
+    # product_detail = scrapy.Field()  # 商品详情，string
+    # product_collected = scrapy.Field()  # 商品收藏，number
+    # product_tags = scrapy.Field()  # 商品标签，string
+    # product_buylimit = scrapy.Field()  #购买限制，number
 
-    # product_purchasedurl = scrapy.Field()  #产品购买详情地址，String
-    # product_insuredurl = scrapy.Field()  #产品爆涨详情地址，String
+    # product_purchasedurl = scrapy.Field()  #产品购买详情地址，string
+    # product_insuredurl = scrapy.Field()  #产品爆涨详情地址，string
 
-    # seller_id = scrapy.Field()  # 店铺编号，String
-    # seller_nick = scrapy.Field()  #店铺名称，String
-    # seller_comp = scrapy.Field()  #公司名称，String
+    # seller_id = scrapy.Field()  # 店铺编号，string
+    # seller_nick = scrapy.Field()  #店铺名称，string
+    # seller_comp = scrapy.Field()  #公司名称，string
 
     '''version2'''
 
-    is_productList = scrapy.Field()  #管道识别，Number
-    is_productInfo = scrapy.Field()  #管道识别，Number
-    is_productHis = scrapy.Field()  #管道识别，Number
+    is_productList = scrapy.Field()  #管道识别，number
+    is_productInfo = scrapy.Field()  #管道识别，number
+    is_productHis = scrapy.Field()  #管道识别，number
 
-    product_id = scrapy.Field()  #商品编号，String
+    product_id = scrapy.Field()  #商品编号，string
+    product_url = scrapy.Field()  #商品地址，string
+    product_maxprice = scrapy.Field()  #商品最高价，number
+    product_minprice = scrapy.Field()  #商品最低价，number
+    product_sellcount = scrapy.Field()  #商品销量，number
+    product_collected = scrapy.Field()  #商品收藏数量，number
+    product_tags = scrapy.Field()  #商品标签，array
+    product_buylimit = scrapy.Field()  #商品购买限制，number
+    product_purchase = scrapy.Field()  #购买数据，bson
+    product_insured = scrapy.Field()  #保障信息，bson
+    time_online = scrapy.Field()  #上线时间，string
+    time_offline = scrapy.Field()  #下线时间，string
+   
+
+
+# '''商品历史信息'''
+
+# class ProductHistoryItem(scrapy.Item):
+
+#     is_productHis = scrapy.Field()  #管道识别，number
     
-    
-
-
-'''商品历史信息'''
-
-class ProductHistoryItem(scrapy.Item):
-
-    is_productHis = scrapy.Field()  #管道识别，Number
-    
-    _id = scrapy.Field()    #商品编号，String
-    data= scrapy.Field()    #历史数据，Object
+#     _id = scrapy.Field()    #商品编号，string
+#     data= scrapy.Field()    #历史数据，Object
 
 
 '''购买记录'''
 
 
 class PurchasedInfoItem(scrapy.Item):
-    is_purchased = scrapy.Field()  # 管道识别，Number
 
-    _id = scrapy.Field()  # 商品编号，String
-    product_name = scrapy.Field()  # 商品名称，String
-    seller_id = scrapy.Field()  # 店铺编号，String
-    seller_name = scrapy.Field()  # 店铺名称，String
+    '''version1'''
+    
+    # is_purchased = scrapy.Field()  # 管道识别，number
 
-    purchased_total = scrapy.Field()  # 销量总计，Object
+    # _id = scrapy.Field()  # 商品编号，string
+    # product_name = scrapy.Field()  # 商品名称，string
+    # seller_id = scrapy.Field()  # 店铺编号，string
+    # seller_name = scrapy.Field()  # 店铺名称，string
 
+    # purchased_total = scrapy.Field()  # 销量总计，Object
 
-'''购买记录（详细）'''
+    '''version2'''
 
+    is_purchased = scrapy.Field()  #管道识别，number
 
-class PurchasedDetailItem(scrapy.Item):
-    is_detail = scrapy.Field()  # 管道识别，Number
+    product_id = scrapy.Field()  #商品编号，string
+    data = scrapy.Field()  #数据容器，json
+    
 
-    _id = scrapy.Field()  # 商品编号，String
-    product_name = scrapy.Field()  # 商品名称，String
-    seller_id = scrapy.Field()  # 店铺编号，String
-    seller_name = scrapy.Field()  # 店铺名称, String
+# '''购买记录（详细）'''
 
-    purchased_daily = scrapy.Field()  # 日销量数据，Object
+# class PurchasedDetailItem(scrapy.Item):
+#     is_detail = scrapy.Field()  # 管道识别，number
 
+#     _id = scrapy.Field()  # 商品编号，string
+#     product_name = scrapy.Field()  # 商品名称，string
+#     seller_id = scrapy.Field()  # 店铺编号，string
+#     seller_name = scrapy.Field()  # 店铺名称, string
+
+#     purchased_daily = scrapy.Field()  # 日销量数据，Object
 
 '''产品保障方案'''
 
 
 class InsuredInfoItem(scrapy.Item):
-    is_insured = scrapy.Field()  # 管道使用，Number
 
-    _id = scrapy.Field()  # 商品编号，String
-    product_name = scrapy.Field()  # 商品名称，String
-    seller_id = scrapy.Field()  # 店铺编号，String
-    seller_name = scrapy.Field()  # 店铺名称，String
+    '''version1'''
 
-    insured_data = scrapy.Field()  # 保障内容，Object
+    # is_insured = scrapy.Field()  # 管道使用，number
+
+    # _id = scrapy.Field()  # 商品编号，string
+    # product_name = scrapy.Field()  # 商品名称，string
+    # seller_id = scrapy.Field()  # 店铺编号，string
+    # seller_name = scrapy.Field()  # 店铺名称，string
+
+    # insured_data = scrapy.Field()  # 保障内容，Object
+    
+    '''version2'''
+
+    is_insured = scrapy.Field()  #管道识别，number
+    
+    product_id = scrapy.Field()  #商品编号，string
+    history_id = scrapy.Field()  #历史信息编号，bson
+    time_online = scrapy.Field()  #上线时间，string
+    time_offline = scrapy.Field()  #下线时间，string
+    data = scrapy.Field()  #数据容器，json
